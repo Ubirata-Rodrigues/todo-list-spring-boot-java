2df7ce1da7fb8600aff99a9ab0a5c69c828e6ab9
Ubiratã Rodrigues <ubirataaz7@gmail.com>
2025-11-03 18:51:14 -0300

    Adiciona dependências iniciais.

---
+++ desafio-todo-list/src/test/java/br/com/ubirataoliveira/desafio_todo_list/DesafioTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ubirataoliveira.desafio_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
