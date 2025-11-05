21e677d4e9e1fcdae1fa707ccaabeb3acb95436a
Ubiratã Rodrigues <ubirataaz7@gmail.com>
2025-11-05 18:54:08 -0300

    Desenvolvimento da Entity do sistema e configuração des seus atributos

---
+++ src/main/java/br/com/ubirataoliveira/desafio_todo_list/entity/Todo.java
@@ -0,0 +1,61 @@
+package br.com.ubirataoliveira.desafio_todo_list.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // O uso do Long se dá pois é um tipo objeto da Wrapper Class que pode ser nulo, e aceita valores maiores que o int primitivo.
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+
+}
