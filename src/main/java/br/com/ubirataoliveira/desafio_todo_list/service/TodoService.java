09927948bb3c24fb2d64f6cf62de4db9b02012bd
Ubiratã Rodrigues <ubirataaz7@gmail.com>
2025-11-05 20:28:29 -0300

    Desenvolvimento da classe de serviço que define os métodos CRUD

---
+++ src/main/java/br/com/ubirataoliveira/desafio_todo_list/service/TodoService.java
@@ -0,0 +1,45 @@
+package br.com.ubirataoliveira.desafio_todo_list.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.ubirataoliveira.desafio_todo_list.entity.Todo;
+import br.com.ubirataoliveira.desafio_todo_list.repository.TodoRepository;
+
+import org.springframework.data.domain.Sort;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    
+    public List<Todo> list(){
+        
+        Sort sort = Sort.by("prioridade").descending().and(
+            Sort.by("nome").ascending());
+            
+            return todoRepository.findAll(sort);
+            
+        }
+        
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+            
+        return list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
